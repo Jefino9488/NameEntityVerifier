e38b649c9f951d4375ace4eae0559730c77c54e9
Jefino9488 <jefinojacob9488@gmail.com>
2024-09-09 23:29:14 +0530

    prompt

--- app.py
+++ app.py
@@ -1,9 +1,46 @@
 from flask import Flask, request, jsonify
+import google.generativeai as genai
+from load_creds import load_creds
 from flask_cors import CORS
+from google.ai.generativelanguage_v1beta.types import content
 import json
 
 app = Flask(__name__)
 CORS(app)
+creds = load_creds()
+genai.configure(credentials=creds)
+
+generation_config = {
+    "temperature": 0.7,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_schema": content.Schema(
+        content.Type.OBJECT,
+        enum={},
+        required={
+            "is_name_verified": True,
+            "doc_type": True,
+            "is_DOB_verified": True,
+            "is_document_verified": True
+        },
+        properties={
+            "is_name_verified": content.Schema(content.Type.BOOLEAN, ),
+            "doc_type": content.Schema(content.Type.STRING, ),
+            "is_address_verified": content.Schema(content.Type.BOOLEAN, ),
+            "is_DOB_verified": content.Schema(content.Type.BOOLEAN, ),
+            "is_father_name_verified": content.Schema(content.Type.BOOLEAN, ),
+            "is_addhar_no_verified": content.Schema(content.Type.BOOLEAN, ),
+            "is_document_verified": content.Schema(content.Type.BOOLEAN, ),
+        },
+    ),
+    "response_mime_type": "application/json",
+}
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return 'Server is running!'
 
 
 @app.route('/verify', methods=['POST'])
@@ -35,6 +72,18 @@ def get_named_entity():
     except Exception as e:
         print(f"An error occurred: {e}")
         return jsonify({"error": str(e)}), 400
+
+
+def crossVerify(combined_data):
+    model = genai.GenerativeModel(
+        model_name="gemini-1.5-pro",
+        generation_config=generation_config,
+        system_instruction="{\n    \"name\": \"\",\n    \"father_name\": \"\",\n    \"dob\": \"\",\n    \"aadhaar_number\": \"\",\n    \"unstructured_data\": \"\"\n}\n\nif the named entities is same as in unstructured return true else return false\nif any of the entity is not matched return document verification as false else true",
+    )
+    prompt = combined_data
+    result = model.generate_content(prompt)
+    print(result)
+    return result
 
 
 if __name__ == '__main__':
